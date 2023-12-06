532b46f8f30adc35281931836b0e07ab1d4eb143
Ameesh <umeskhan700@gmail.com>
2023-12-06 04:15:55 -0800

    1st

---
+++ PointOfSale.Model/Bank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PointOfSale.Model
+{
+    public partial class Bank
+    {
+        public Bank()
+        {
+			Customers = new HashSet<Customer>();
+        }
+
+        public int IdCategory { get; set; }
+        public string? Description { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
